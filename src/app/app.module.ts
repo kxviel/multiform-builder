3d4c59b5b104e2e4939ef4fbf904278c6faa1546
Kevin D Goveas <kevdanngovead@gmail.com>
2023-08-19 16:10:02 +0530

    project base setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './main/app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
 })
 export class AppModule {}
